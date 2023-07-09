cf8b87bc73458ddd67c34c10cf7c0c1ce29449d5
Mazen Kamel <mazenwkamel@gmail.com>
2023-07-09 12:47:07 +0300

    Update filtering function to include extensive error handling and better tyoe detection

---
+++ Exceptions/UtilTrackingExceptions.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_Tracker.Exceptions
+{
+    internal class UtilFilterException : Exception
+    {
+        public UtilFilterException(string? message = default, Exception? innerException = default) : base(message, innerException) { }
+    }
+
+    internal class UtilFilterCriteriaException : ArgumentException
+    {
+        public object? Criteria { get; set; }
+
+        public UtilFilterCriteriaException(string? message = default, object? criteria = default, string? paramName = default,
+            Exception? innerException = default) : base(message, paramName, innerException)
+        {
+            Criteria = criteria;
+        }
+    }
+
+    internal class UtilSortException : Exception
+    {
+        public UtilSortException(string? message = default, Exception? innerException = default) : base(message, innerException) { }
+    }
+}
